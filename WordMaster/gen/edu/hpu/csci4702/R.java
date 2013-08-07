e5c9406fe5dc51fcba8e491a646a3c9a58d76b13
Precious <pbinas@gmail.com>
2013-08-06 20:10:04 -1000

    Changed name of the game to reflect actual name
    Exported a new .apk and deleted old one inside bin

--- WordMaster/gen/edu/hpu/csci4702/R.java
+++ WordMaster/gen/edu/hpu/csci4702/R.java
@@ -60,6 +60,7 @@ public final class R {
         public static final int trues=0x7f040008;
     }
     public static final class string {
+        public static final int anagramix=0x7f050002;
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
     }
